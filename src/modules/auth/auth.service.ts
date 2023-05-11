1c9a37d0125688037009bdec46653918e6901ee6
manndev <augusto.mannarini@acapuedo.com>
2023-05-10 21:41:14 -0400

    errorCath interceptor improved

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -29,6 +29,7 @@ export class AuthService {
 
     async login(loginUserDto: LoginUserDto) {
         const { password, email } = loginUserDto;
+        throw new UnauthorizedException("Credentials are not valid", {cause: Error("causa"), description: "desc"});
         const user = await this.userRepository.findOne({where: {email}, select: {email: true, password: true, id: true}});
         if(!user || !bcrypt.compareSync(password, user.password) ) throw new UnauthorizedException("Credentials are not valid");
 
--- src/modules/common/interceptos/error-catch.interceptor.ts
+++ src/modules/common/interceptos/error-catch.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, InternalServerErrorException, HttpException } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, InternalServerErrorException, HttpException, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -7,7 +7,15 @@ import { catchError } from 'rxjs/operators';
 export class ErrorCatch implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         return next.handle()
-            .pipe(catchError((err: any) => {
+        .pipe(catchError((err: any) => {
+            const { body: reqBody } = context.switchToHttp().getRequest();
+            const {name: methodName} = context.getHandler()
+
+            if(reqBody) {
+                if(reqBody.password) delete reqBody.password;
+            }
+
+            Logger.error(`${err}\nReqBody: ${JSON.stringify(reqBody ?? {})}`, `METHOD: ${methodName}`);
                 if(!(err instanceof HttpException)) {
                     console.log(err);
                     throw new InternalServerErrorException("Internal server error - check logs");
