dafd3cfebf1ad89c42dc1f7495d344144e2ac190
manndev <augusto.mannarini@acapuedo.com>
2023-05-10 22:15:27 -0400

    fix, remove login err

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -29,7 +29,6 @@ export class AuthService {
 
     async login(loginUserDto: LoginUserDto) {
         const { password, email } = loginUserDto;
-        throw new UnauthorizedException("Credentials are not valid", {cause: Error("causa"), description: "desc"});
         const user = await this.userRepository.findOne({where: {email}, select: {email: true, password: true, id: true}});
         if(!user || !bcrypt.compareSync(password, user.password) ) throw new UnauthorizedException("Credentials are not valid");
 
